27d14448af8413692839b1c4322990cf429a0495
Lara Bagdasarian <Lara Bagdasarian>
2016-03-10 12:42:11 -0800

    template

---
+++ TypeScriptHTMLApp3/TypeScriptHTMLApp3/app.ts
@@ -0,0 +1,19 @@
+﻿module Game {
+    export class TypeScriptHTMLApp3 {
+        game: Phaser.Game;
+
+        constructor() {
+            this.game = new Phaser.Game(1280, 720, Phaser.AUTO, 'content', {
+                create: this.create, preload: this.preload
+            });
+        }
+        preload() {
+        }
+        create() {
+        }
+    }
+}
+
+window.onload = () => {
+    var game = new Game.TypeScriptHTMLApp3();
+};
