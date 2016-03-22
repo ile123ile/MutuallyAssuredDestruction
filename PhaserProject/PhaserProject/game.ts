0d0d97d14a1cffcfcbf19dfcab489f6bda5f353a
themost1 <benjaminmost@gmail.com>
2016-03-22 12:41:26 -0700

    Shooting and enemies

--- PhaserProject/PhaserProject/game.ts
+++ PhaserProject/PhaserProject/game.ts
@@ -8,17 +8,23 @@ var time=0;
 var timeText;
 var inputText;
 var maxTime = 0;
+var enemyArray = [];
+var occupied = []; // xcoord + 1000000* ycoord
+var role = "shooting";
+var bullets;
+var bulletVel = 900;
 
 function preload() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
     // add our logo image to the assets class under the
     // key 'logo'. We're also setting the background colour
     // so it's the same as the background colour in the image
     game.stage.backgroundColor = 0xffffff;
-    this.game.load.image('grid', "Graphics/2151465-grid.jpg");
-    this.game.load.image('player', "Graphics/greendude.png");
-    cursors = this.game.input.keyboard.createCursorKeys();
-    window.setInterval(submitMove, 3000);
-    window.setInterval(updateTimer, 300);
+    this.game.load.image('grid', 'Graphics/2151465-grid.jpg');
+    this.game.load.image('player', 'Graphics/greendude.png');
+    this.game.load.image('bullet', 'Graphics/bullet.png');
+    this.game.load.image('redEnemy', "Graphics/redenemy.jpg");
 }
 
 function create() {
@@ -32,6 +38,39 @@ function create() {
     var grid = this.game.add.sprite(0, 100, 'grid');
     player = this.game.add.sprite(0, 100, 'player');
     player.scale.setTo(0.05, 0.05);
+
+    //init enemies
+    var a = Math.random();
+    var b = Math.random();
+    for (var i = 0; i < 3; i++) {
+        //generate random coordinates
+        a = Math.random();
+        b = Math.random();
+        for (var count = 0; count < enemyArray.length; count++) {
+            while ((enemyArray[count].x == a && enemyArray[count].y == b) || (a == player.x && b == player.y)) {
+                a = Math.random();
+                b = Math.random();
+            }
+        }
+        //  var tempEnemy = (this.game.add.sprite(travelDist * randomX, travelDist * randomY+100, 'redEnemy'));
+        occupied.push(4 + (travelDist - .1) * Math.round(a * 14) + 1000000 * (100 + 4 + (travelDist - .1) * Math.round(b * 14)));
+        var tempEnemy = (this.game.add.sprite(4 + (travelDist - .1) * Math.round(a * 14), 100 + 4 + (travelDist - .1) * Math.round(b * 14), 'redEnemy'));
+        tempEnemy.scale.setTo(0.3, 0.3);
+        enemyArray.push(tempEnemy);
+    }
+
+
+    bullets = game.add.group();
+    bullets.enableBody = true;
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+
+    bullets.createMultiple(50, 'bullet');
+    bullets.setAll('checkWorldBounds', true);
+    bullets.setAll('outOfBoundsKill', true);
+
+    cursors = this.game.input.keyboard.createCursorKeys();
+    window.setInterval(submitMove, 3000);
+    window.setInterval(updateTimer, 300);
 }
 
 function updateTimer() {
@@ -40,16 +79,127 @@ function updateTimer() {
 }
 
 function submitMove() {
-    if (movementInput == "right")
-        player.x += travelDist;
-    else if (movementInput == "left")
-        player.x -= travelDist;
-    else if (movementInput == "up")
-        player.y -= travelDist;
-    else if (movementInput == "down")
-        player.y += travelDist;
-    movementInput = "";
+    moveEnemy();
+
     time = 0;
+    if (role == "moving") {
+        if (movementInput == "right")
+            player.x += travelDist;
+        else if (movementInput == "left")
+            player.x -= travelDist;
+        else if (movementInput == "up")
+            player.y -= travelDist;
+        else if (movementInput == "down")
+            player.y += travelDist;
+    }
+    else if (role == "shooting") {
+        
+        if (movementInput != "") {
+
+            var bullet = bullets.getFirstDead();
+
+
+            bullet.reset(player.x+player.width/2-bullet.width/2, player.y+player.height/2-bullet.height/2);
+
+            if (movementInput == "right")
+                bullet.body.velocity.x = bulletVel;
+            else if (movementInput == "left")
+                bullet.body.velocity.x = -1*bulletVel;
+            else if (movementInput == "up")
+                bullet.body.velocity.y = -bulletVel;
+            else if (movementInput == "down")
+                bullet.body.velocity.y = bulletVel;
+        }
+    }
+
+    movementInput = "";
+
+    for (var r = 0; r < enemyArray.length; r++) {
+        if ((Math.abs(player.x - enemyArray[r].x) <= 10) && (Math.abs(player.y - enemyArray[r].y) <= 10)) {
+            player.kill(); //remove player from grid
+        }
+    }
+    time = 0;
+}
+
+function moveEnemy() {
+    //update enemies' moves
+    var tempXpos, tempYpos;
+    for (var count = 0; count < enemyArray.length; count++) {
+        tempXpos = enemyArray[count].x;
+        tempYpos = enemyArray[count].y;
+        if (Math.abs(player.y - enemyArray[count].y) < 10) {
+            if (enemyArray[count].x > player.x)
+                enemyArray[count].x -= (travelDist - .1);
+            else
+                enemyArray[count].x += (travelDist - .1);
+        }
+        else if (Math.abs(player.x - enemyArray[count].x) < 10) {
+            if (enemyArray[count].y > player.y)
+                enemyArray[count].y -= (travelDist - .1);
+            else
+                enemyArray[count].y += (travelDist - .1);
+        }
+        else if (Math.abs(Math.atan((player.y - enemyArray[count].y) / (player.x - enemyArray[count].x))) > (Math.PI / 4)) {
+            if (enemyArray[count].y < player.y)
+                enemyArray[count].y += (travelDist - .1);
+            else
+                enemyArray[count].y -= (travelDist - .1);
+        }
+        else {
+            if (enemyArray[count].x < player.x)
+                enemyArray[count].x += (travelDist - .1);
+            else
+                enemyArray[count].x -= (travelDist - .1);
+        }
+        //make sure no two enemies occupy the same location
+        var tries = 0;
+        while (contains.call(occupied, enemyArray[count].x + 1000000 * enemyArray[count].y)) {
+            if (tries == 0)
+                enemyArray[count].x = tempXpos - (travelDist - .1); //move it sideways -- add checking for out of bounds
+            else if (tries == 1)
+                enemyArray[count].x = tempXpos + (travelDist - .1);
+            else if (tries == 2)
+                enemyArray[count].y = tempYpos - (travelDist - .1);
+            else if (tries == 3)
+                enemyArray[count].y = tempYpos + (travelDist - .1);
+            else {
+                enemyArray[count].x = tempXpos;
+                enemyArray[count].y = tempYpos;
+            }
+            tries++;
+        }
+        occupied[count] = enemyArray[count].x + 1000000 * enemyArray[count].y;
+    }
+}
+var contains = function (needle) {
+    // Per spec, the way to identify NaN is that it is not equal to itself
+    var findNaN = needle !== needle;
+    var indexOf;
+    if (!findNaN && typeof Array.prototype.indexOf === 'function') {
+        indexOf = Array.prototype.indexOf;
+    }
+    else {
+        indexOf = function (needle) {
+            var i = -1, index = -1;
+            for (i = 0; i < this.length; i++) {
+                var item = this[i];
+                if ((findNaN && item !== item) || item === needle) {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
+        };
+    }
+    return indexOf.call(this, needle) > -1;
+};
+
+function resetBullet(bullet) {
+
+    //  Called if the bullet goes out of the screen
+    bullet.kill();
+
 }
 
 function update() {
@@ -65,5 +215,11 @@ function update() {
     if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
         movementInput = "right";
     }
-    inputText.setText(movementInput);
+    if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+        if (role == "moving")
+            role = "shooting";
+        else if (role == "shooting")
+            role = "moving";
+    }
+    inputText.setText(movementInput+"   "+role);
 }
